fd4c0770ff7f74acdb5f6ea3c24512ad49d620c0
AnirudhSinghRajora <71185228+AnirudhSinghRajora@users.noreply.github.com>
2021-01-04 17:16:46 +0530

    wtf
    wtf

--- Darkbot.py
+++ Darkbot.py
@@ -1,5 +1,4 @@
 import discord
 from discord.ext import commands
-token = 'NzY1MDg3Mzc2OTQxOTA4MDA4.X4PtJQ.o4ww6_d4gWO_MlIWE7HJieGDq5w'
 bot = commands.Bot(command_prefix="^")
-bot.run("token")
+bot.run("Nzk1Mjk1MDQxMDk3Njk1MjYy.X_HSOQ.hTgqQc_laJYmqoC53PdHRn0RWFI")

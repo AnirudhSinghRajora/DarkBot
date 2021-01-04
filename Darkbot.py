8a1d0acc465a3fd2327c81b523b5ca03d4f07a16
AnirudhSinghRajora <71185228+AnirudhSinghRajora@users.noreply.github.com>
2021-01-04 20:38:12 +0530

    rudra op
    op

--- Darkbot.py
+++ Darkbot.py
@@ -4,23 +4,47 @@ import json
 import urllib.request
 import random
 token = 'Nzk1Mjk1MDQxMDk3Njk1MjYy.X_HSOQ.CAhkE9uOrM3UoAg5CZN4QSyrj-E'
-client = discord.Client()
 
 
-@client.event
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
+@bot.event
 async def on_ready():
    automeme.start()
    print("bot is online")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.lower().startswith('hello'):
+        await message.channel.send('Hello!')
+    elif message.content.lower().startswith('evil laugh'):
+        await message.channel.send('https://tenor.com/view/the-grinch-evil-smile-creepy-smile-gif-7618321')
+    elif message.content.lower().startswith('abe saale'):
+        await message.channel.send('https://tenor.com/view/abe-saale-sabbir31x-harmonium-sale-gif-15727715')
+    elif message.content.lower().startswith('mar saale ko'):
+        await message.channel.send('https://tenor.com/view/moumita-khopdi-tod-re-saale-ka-pointing-gif-15226261')
+       
+
+@bot.command(ping)
+ async def ping(ctx):
+    await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
+
+
+
+
 @tasks.loop(minutes=10)
 async def automeme():
-    
     url = "https://meme-api.herokuapp.com/gimme/dankmemes"
     response = urllib.request.urlopen(url)      
     data = json.loads(response.read()) 
@@ -30,33 +54,12 @@ async def automeme():
     memebed = discord.Embed(title=img_title,url=img_url,color=random.randint(0,0xffffff))
     
     memebed.set_image(url=img_url)
-    meme_channel = client.get_channel(764758915292332032)
+    meme_channel = bot.get_channel(764758915292332032)
     await meme_channel.send(embed=memebed)
+@automeme.before_loop
+async def beforeautomeme():
+    print("Waiting Until Ready...")
+    await bot.wait_until_ready()
+    print("Bot Ready!")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
-
-
-
-
-     
-
-    if message.content.startswith('evil laugh'):
-        await message.channel.send('https://tenor.com/view/the-grinch-evil-smile-creepy-smile-gif-7618321')
-
-
-    if message.content.startswith('abe saale'):
-        await message.channel.send('https://tenor.com/view/abe-saale-sabbir31x-harmonium-sale-gif-15727715')
-    
-
-
-
-    if message.content.startswith('Mar saale ko'):
-        await message.channel.send('https://tenor.com/view/moumita-khopdi-tod-re-saale-ka-pointing-gif-15226261')
-       
-client.run(token)
+bot.run(token)

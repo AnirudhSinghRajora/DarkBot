ee095a06fb54b13d90a08ede32ab70aa1f5d4c0c
AnirudhSinghRajora <71185228+AnirudhSinghRajora@users.noreply.github.com>
2021-01-04 18:38:21 +0530

    Thanks Kaushik
    Welcome tho

--- Darkbot.py
+++ Darkbot.py
@@ -1,5 +1,8 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
+import json
+import urllib.request
+import random
 token = 'Nzk1Mjk1MDQxMDk3Njk1MjYy.X_HSOQ.CAhkE9uOrM3UoAg5CZN4QSyrj-E'
 client = discord.Client()
 
@@ -10,5 +13,26 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+@client.event
+async def on_ready():
+   automeme.start()
+   print("bot is online")
+@tasks.loop(minutes=3)
+async def automeme():
+    
+    url = "https://meme-api.herokuapp.com/gimme/dankmemes"
+    response = urllib.request.urlopen(url)      
+    data = json.loads(response.read()) 
+    img_url = data['url']   
+    post_link = data['postLink']
+    img_title = data['title']
+    memebed = discord.Embed(title=img_title,url=img_url,color=random.randint(0,0xffffff))
+    
+    memebed.set_image(url=img_url)
+    meme_channel = client.get_channel(764758915292332032)
+    await meme_channel.send(embed=memebed)
+
+
 
 client.run(token)

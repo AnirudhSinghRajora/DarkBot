248fc29b464e1f9f2bfa23a110d6078d2edea3c6
AnirudhSinghRajora <71185228+AnirudhSinghRajora@users.noreply.github.com>
2021-01-04 17:28:43 +0530

    shit
    wow

--- Darkbot.py
+++ Darkbot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-token = 'NzY1MDg3Mzc2OTQxOTA4MDA4.X4PtJQ.o4ww6_d4gWO_MlIWE7HJieGDq5w'
+token = 'Nzk1Mjk1MDQxMDk3Njk1MjYy.X_HSOQ.CAhkE9uOrM3UoAg5CZN4QSyrj-E'
 client = discord.Client()
 
 

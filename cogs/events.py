00fea94678eaa57a053ca6cb750808dc1aaeacf7
AnirudhSinghRajora <71185228+AnirudhSinghRajora@users.noreply.github.com>
2021-01-28 15:16:04 +0530

    asda
    sdasd

--- cogs/events.py
+++ cogs/events.py
@@ -12,6 +12,14 @@ class events(commands.Cog):
     @commands.command()
     async def ping(self,ctx):
         await ctx.send(f'pong! {round(self.bot.latency * 1000)}ms')
+    @commands.command()
+    async def evil(self,ctx):
+        await ctx.send("https://tenor.com/view/the-grinch-evil-smile-creepy-smile-gif-7618321")
+
+
+    @commands.command()
+    async def abe_saale(self,ctx):
+        await ctx.send("https://tenor.com/view/abe-saale-sabbir31x-harmonium-sale-gif-15727715")
         
 def setup(bot):
     bot.add_cog(events(bot))
